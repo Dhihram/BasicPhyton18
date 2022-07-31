a43492191523e5481e049766955b296f531c508f
Dhihram <105160118+Dhihram@users.noreply.github.com>
2022-07-31 15:01:00 +0700

    tambahan hari kedua

---
+++ meeting#2/string_formattin.py
@@ -0,0 +1,14 @@
+nama = "Andi"
+umur = 35
+
+biodata = "nama saya adalah {1}, saya berumur {0} tahun.".format(nama,umur)
+print(biodata)
+
+a = 100
+b = 200
+c = 200
+
+if not a < b:
+    print("a lebih kecil dari b")
+else:
+    print("program selesai")
