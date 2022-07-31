c0c8e8ea9686b5176d8cf1e835266dad1afbf5f5
Dhihram <105160118+Dhihram@users.noreply.github.com>
2022-07-31 14:00:18 +0700

    ada tamabahan

--- hello.py
+++ hello.py
@@ -1,3 +1,18 @@
 print("hello world")
 print(1+1)
 print(2+1)
+x = 5
+y = "Hello World"
+print(x)
+print(y)
+
+print("===========================")
+x = 5
+y = 10
+print("============================")
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**y)
+print(y%x)
